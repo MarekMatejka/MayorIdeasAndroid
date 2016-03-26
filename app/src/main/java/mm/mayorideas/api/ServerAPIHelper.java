5c260327ab1b70998956f804038b8057d2eed699
MarekMatejkaKCL <marek.matejka@kcl.ac.uk>
2016-03-26 00:23:13 +0000

    fixing an issue of where the app is looking for server and increasing version

--- app/src/main/java/mm/mayorideas/api/ServerAPIHelper.java
+++ app/src/main/java/mm/mayorideas/api/ServerAPIHelper.java
@@ -2,10 +2,10 @@ package mm.mayorideas.api;
 
 public final class ServerAPIHelper {
 
-    private static final String SERVER = "http://10.193.140.181:8015/"; //TESTING
+    //private static final String SERVER = "http://10.193.140.181:8015/"; //TESTING
     //private static final String SERVER = "http://192.168.1.39:8015/"; //DOMA
     //private static final String SERVER = "http://10.40.228.218:8015/"; //SKOLA
-    //private static final String SERVER = "http://52.49.91.43:8015/"; //AWS
+    private static final String SERVER = "http://52.49.91.43:8015/"; //AWS
 
     public static String getServer() {
         return SERVER;
